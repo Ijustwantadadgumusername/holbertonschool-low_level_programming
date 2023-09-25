58287ad01f9ffaec198668b8e80b45f7e9aad61b
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-09-25 16:46:59 -0700

    4-print_alpabt.c

--- variables_if_else_while/4-print_alpabt.c
+++ variables_if_else_while/4-print_alpabt.c
@@ -4,23 +4,21 @@
 
 /**
  * main - Entry point
- * @void: Null value
- *
- * Description: Print alabet execpt e q
- * Return: Zero value
+ * Return: Always 0 (Success)
  */
-
 int main(void)
+
 {
+
 	char low, e, q;
 
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a'; low = 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != e && low != q)
-		putchar(low);
+	if (low != e && low != q)
+	putchar(low);
 	}
 	putchar('\n');
 	return (0);
