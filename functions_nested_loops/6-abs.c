655823519d1964a86977f9b9752dfc36665cd027
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-05 16:21:43 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an interger
+ * @i: i think i need one of these so I chose i for Isaiah
+ * Return: return 0
+ */
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
+	else
+		return (0);
+}
