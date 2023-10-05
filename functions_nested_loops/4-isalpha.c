59d7dad81f379ad368a26d667d3b4b7421960033
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-05 15:30:05 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  */
 int _isalpha(int c)
 {
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-        {
-                return (1);
-        }
-        return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
 }
