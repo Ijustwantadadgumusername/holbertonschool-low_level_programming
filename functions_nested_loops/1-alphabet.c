931390c6113293bde0ed36e00ad42bb8a29c418c
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-05 12:01:09 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(aplhabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
