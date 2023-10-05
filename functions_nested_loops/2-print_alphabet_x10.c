7b4859477d08b3974462d250962c1f665672ed50
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-05 12:31:36 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include 'main.h'
+
+/**
+ * print_alphabet_x10 - take a guess
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabetx10;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	
+	for (alphabetx10 = 'a'; alphabetx10 <= 'z'; alphabetx10++)
+	{
+		_putchar(alphabetx10);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
+
