664b9f44d753e8af1bb738a383602910960053b0
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-06 10:56:26 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0-9 followed by new line
+ * Return: returns nothing
+ */
+void print_numbers(void)
+{
+int n = 0;
+for (n = 0; n < 58; n++)
+{
+	_putchar(n);
+}
+_putchar(\n);
+}
