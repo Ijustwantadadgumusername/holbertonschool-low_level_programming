b27a1f8e76ed44f26d0c62282664e041337d3197
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-05 18:10:32 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - what do you think add does
+ * @lb: Larry Bird
+ * @mj: Magic Johnson
+ * if you add these two together you get the saviours of basketball
+ * Return: returns 0
+ */
+int add(int lb, int mj)
+{
+	return (lb + mj);
+}
