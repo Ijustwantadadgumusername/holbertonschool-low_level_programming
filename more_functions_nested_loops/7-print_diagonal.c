f80f53ca6f8fac444a67d6a8d260bc29325c93c3
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-06 12:33:55 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - take a guess
+ * @n: integer used
+ * Return: returns 0
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		_putchar('\');
+	_putchar('\n')
+}
