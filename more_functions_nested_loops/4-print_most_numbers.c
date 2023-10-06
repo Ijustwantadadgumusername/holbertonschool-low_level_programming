cb4532531534e9a60b543ce3af670f012c33bf77
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-06 11:33:30 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints all but 2 and 4
+ * Return: returns nothing
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		if ((x == 50) || (x == 52))
+		{
+			continue;
+		}
+		_putchar(x);
+	}
+	_putchar(10);
+}
