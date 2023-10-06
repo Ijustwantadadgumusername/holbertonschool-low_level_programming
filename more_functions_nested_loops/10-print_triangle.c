c7a7c691574edbd30b8b6e6ba7e9a0f7a34dcae8
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-06 14:22:34 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints a dadgum triangle
+ * @size: size of dadgum triangle
+ * Return: returns nothing
+ */
+void print_triangle(int size)
+{
+	int r;
+	int s;
+	int p;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (s = size - r; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (p = 1; p <= r; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
