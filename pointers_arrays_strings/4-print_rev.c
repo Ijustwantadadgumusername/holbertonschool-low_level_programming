a06bfb9cbbcbcee513dce5c912e34bdf7445d5a5
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-10 13:24:00 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints in reverse
+ * @s: integer used
+ * Return: returns void
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
