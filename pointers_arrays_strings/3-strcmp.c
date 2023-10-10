7554aff090ff863eb73eec19feefe377f20c797d
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-10 15:54:39 -0700

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _strcmp - compares 2 strings
+ * @s1: first strinf
+ * @s2: second string
+ * Return: returns int value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int r = 0;
+	int l;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		l = a;
+	}
+	else
+	{
+		l = b;
+	}
+	while (c <= l)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+	return (r);
+}
