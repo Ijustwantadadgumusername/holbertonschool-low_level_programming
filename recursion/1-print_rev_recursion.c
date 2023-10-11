8083a706d9600292ab992aa5f8c95cfb7d5f8878
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-11 14:16:55 -0700

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: said string
+ * Return: returns void
+ */
+void print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		reverse(s + 1);
+		_putchar(*s);
+	}
+}
