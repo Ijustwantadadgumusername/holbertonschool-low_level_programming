73029830d05c7e2be50f3ca45c34a9cc3f51faef
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-16 21:44:16 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds postive numbers
+ * @argc: counts the arguments
+ * @argv: where the arguments are
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int z;
+	int sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (z = 0; argv[a][z] != '\0'; z++)
+		{
+			if (argv[a][z], '0' || argv[a][z] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[1]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
