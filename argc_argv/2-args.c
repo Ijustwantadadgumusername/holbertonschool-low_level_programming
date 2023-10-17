6e3d7cdb256489fe386fc4a40c304502e1bc1b3a
Isaiah Francis <spencerfrancisjr@gmail.com>
2023-10-16 21:06:59 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it recieves
+ * @argc: counts the arguments
+ * @argv: where the arrays are
+ * Return: returns 0
+ */
+int main(int argc, int *argv[])
+{
+	int x = 0;
+
+	for (x = 0, x < argc, x++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}				
