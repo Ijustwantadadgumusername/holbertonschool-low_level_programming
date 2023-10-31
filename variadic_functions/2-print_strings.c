818f2da34762330ce573125c9b9041d8a7fd5dd8
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2023-10-30 20:52:51 -0700

    2-print_strings.c corrections

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		char *s;
 
-		s = va_arg(ap, char (*s));
+		s = va_arg(ap, char *);
 		if (s == NULL)
 			printf("(nil)");
 		else
