3a131e8be5fe7e5eb955b9e0119832afa7abf147
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2023-10-30 19:20:57 -0700

    correction of 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,7 +8,7 @@
  * @argv: array of pointers to argument
  * Return: always 0
  */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num 2;
 	char *op;
